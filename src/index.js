83531eb55558440bb25b4ed64e222c90e01bc1cd
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-06-25 16:51:24 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector("input#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
+
+input.addEventListener("input", debounce(onInputTape), DEBOUNCE_DELAY);
+
+function onInputTape() {
+    
+    countryList.innerHTML = '';
+
+    const valueInput = input.value.trim();
+    console.log(valueInput);
+
+    fetchCountries(valueInput).then(countries => {
+        // console.log(countryInfo.innerHTML = buildCountryMarkup(countries)));
+    
+        const countryLength = countries.length;
+
+        if (countryLength === 1) {
+            console.log(countries);
+            countryInfo.innerHTML = buildCountryMarkup(countries);
+        } else if (countryLength > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        } else {
+            countryList.innerHTML = buildCountryList(countries);
+        }
+    })
+        .catch(error => Notiflix.Notify.failure("Oops, there is no country with that name"));
+}
+
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => { 
+            if (!response.ok) {
+        throw new Error(response.statusText);
+            }
+            return response.json();
+            });
+};
+
+function buildCountryList(countries) {
+    return countries.map(country => `<li>
+        <img src=${country.flags.svg} height=12/>
+        <span>${country.name.official}</span>
+        </li>`).join('');
+}
+
+function buildCountryMarkup(countries) {
+
+    
+    return countries.map(({ name, capital, population, flags, languages }) => {
+        const lang = JSON.stringify(languages);
+        `<ul>
+        <li class="country-list-header">
+        <img class="country-list-img" src=${flags.svg} height=18/>
+        <span class="country-title">${name.official}</span>
+        </li>
+        <li class="country-list-item">
+        <span class="country-name-inform">Capital: </span><span class="country-definition">${capital}</span>
+        </li>
+        <li class="country-list-item">
+        <span class="country-name-inform">Population: </span><span class="country-definition">${population}</li>
+        <li class="country-list-item">
+        <span class="country-name-inform">Languages: </span><span class="country-definition">${lang}</span>
+        </li>
+            </ul>`}).join("");
+}        
